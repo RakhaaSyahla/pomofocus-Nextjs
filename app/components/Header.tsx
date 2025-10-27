759c7a56bdf1045d3575991e15fd87a50b8b5a67
Rakha <syahlarakha2006@gmail.com>
2025-10-27 21:08:43 +0700

    hapus coment

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,13 @@
 // app/components/Header.tsx
 
-import Link from 'next/link';
-import styles from './Header.module.css';
+import Link from "next/link";
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <Link href="/" className={styles.logo}>
-          {/* Kamu bisa ganti ini dengan logo/icon nanti */}
           Pomofocus
         </Link>
         <nav>
